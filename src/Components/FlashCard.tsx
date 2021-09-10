12be458c951b2bb084bf1100a084269cfad45c11
Goku-kun <jethvadharmarajsinh@gmail.com>
2021-09-10 11:29:28 +0530

    🔥 UPDATE: firebase working, 🧹 DEL: json implementation

--- src/Components/FlashCard.tsx
+++ src/Components/FlashCard.tsx
@@ -1,19 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Paper, Button } from "@material-ui/core"
 import "./FlashCard.css";
 import { ArrowBackIosRounded, ArrowForwardIosRounded } from '@material-ui/icons';
+import { word } from '../utils/getwords';
 
 interface Properties {
-  words: ({ word: string, meaning: string[], synonyms: string[], antonyms: string[], examples: string[], pos: string, origin?: string })[],
-  seenWords: any[],
-  setSeenWords: any
+  words: word[]
 }
 
-function FlashCard({ words, seenWords, setSeenWords }: Properties) {
-  const [index, setIndex] = useState(Math.floor(Math.random() * words.length));
-  const [previousIndex, setPreviousIndex] = useState(index);
-  const [word, setWord] = useState(words[index]);
-
+function FlashCard({ words }: Properties) {
+  const [index, setIndex] = useState(0);
+  const [previousIndex, setPreviousIndex] = useState(0);
+  const [word, setWord]: [any, any] = useState({});
 
   function flipCard(event: React.MouseEvent): void {
     event.preventDefault();
@@ -22,7 +20,13 @@ function FlashCard({ words, seenWords, setSeenWords }: Properties) {
     card.classList.toggle("flip");
   }
 
-  function newWord(e: React.MouseEvent): void {
+  useEffect(function () {
+    if (!(words.length === 0)) {
+      setWord(words[Math.floor(Math.random() * words.length)])
+    }
+  }, [words])
+
+  async function newWord(e: React.MouseEvent): Promise<any> {
     var card = document.querySelector(".card");
     if (card !== null) {
       card.classList.remove("flip");
@@ -34,7 +38,8 @@ function FlashCard({ words, seenWords, setSeenWords }: Properties) {
     setPreviousIndex(index);
     setIndex(newerIndex);
 
-    setSeenWords([word, ...seenWords]);
+    // var docRef = await addDoc(collection(db, "words"), words[newerIndex]);
+    // console.log(docRef);
 
     setWord(words[newerIndex]);
   }
@@ -47,32 +52,37 @@ function FlashCard({ words, seenWords, setSeenWords }: Properties) {
     setIndex(previousIndex);
     setWord(words[previousIndex]);
   }
+  if (words.length === 0 || Object.keys(word).length === 0) {
+    return (
+      <>
+        <h1>Loading</h1>
+      </>
+    );
+  }
 
   return (
     <>
-      <Paper elevation={ 3 }>
-        <div className="card-container">
-          <div className="card" onClick={ flipCard }>
-            <div className="face front">
-              <h2>{ word.word }</h2>
-            </div>
-            <br />
-            <div className="face back">
-              <ul className="main">
-                <li><strong>Meaning:</strong></li>
-                <ul>{ word.meaning.map(w => <li className="no-style" key={ w }>{ w }</li>) }</ul>
-                <li><strong>Part of Speech</strong>: { word.pos }</li>
-                <li><strong>Synonyms</strong>: { word.synonyms.join(", ") }</li>
-                <li><strong>Antonyms</strong>: { word.antonyms.join(", ") }</li>
-                <li><strong>Example: </strong>
-                  <br />
-                  <ul>
-                    { word.examples.map(eg => <li key={ eg }>{ eg }</li>) }
-                  </ul>
-                </li>
-                { word.origin && <li><strong>Origin:</strong>{ word.origin }</li> }
-              </ul>
-            </div>
+      <Paper elevation={ 3 } style={ { width: "85vw" } }>
+        <div className="card" onClick={ flipCard }>
+          <div className="face front">
+            <h2>{ word.word }</h2>
+          </div>
+          <br />
+          <div className="face back">
+            <ul className="main">
+              <li><strong>Meaning:</strong></li>
+              <ul>{ word.meaning.map((w: string) => <li className="no-style" key={ w }>{ w }</li>) }</ul>
+              <li><strong>Part of Speech</strong>: { word.pos }</li>
+              <li><strong>Synonyms</strong>: { word.synonyms.join(", ") }</li>
+              <li><strong>Antonyms</strong>: { word.antonyms.join(", ") }</li>
+              <li><strong>Example: </strong>
+                <br />
+                <ul>
+                  { word.examples.map((eg: any) => <li key={ eg }>{ eg }</li>) }
+                </ul>
+              </li>
+              { word.origin && <li><strong>Origin:</strong>{ word.origin }</li> }
+            </ul>
           </div>
         </div>
       </Paper>
---
+++ src/utils/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAKvKwVUGYmViXf97oL-qmd5OwZ1KhYxcM",
+  authDomain: "gre-flash-cards-34c9f.firebaseapp.com",
+  projectId: "gre-flash-cards-34c9f",
+  storageBucket: "gre-flash-cards-34c9f.appspot.com",
+  messagingSenderId: "482893921001",
+  appId: "1:482893921001:web:78c317b3c7c1cb35824e78"
+};
+
+export const firebaseApp = initializeApp(firebaseConfig);
+
+export const db = getFirestore();
